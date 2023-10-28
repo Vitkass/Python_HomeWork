5b72165d17e6e598070641ed6da9d3ce928ae9b4
VPSergeev <vpsergeev@it.ru>
2023-10-28 21:36:53 +0300

    delete all

--- project/utils/extract_prices.py
+++
@@ -1,6 +0,0 @@
-def extract_prices(products:list)->list:
-    prices = []
-    for product in products:
-        prices.append(product.price)
-    
-    return prices
--- project/utils/select_products_by_category.py
+++
@@ -1,7 +0,0 @@
-def select_products_by_category(products:list, category:str)->list:
-    select_products = []
-    for product in products:
-        if product.category == category:
-            select_products.append(product)
-    
-    return select_products

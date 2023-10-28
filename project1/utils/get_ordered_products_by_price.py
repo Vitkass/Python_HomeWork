349cf15502ca11e059235d2e4d77a592bd540288
VPSergeev <vpsergeev@it.ru>
2023-10-28 21:22:30 +0300

    delete from main

--- project1/utils/get_ordered_products_by_price.py
+++
@@ -1,6 +0,0 @@
-def price_sorted(product):
-    return product.price
-
-
-def get_ordered_products_by_price(products):
-    return sorted(products, key=price_sorted, reverse=True)
--- project1/utils/select_top_users_by_rate.py
+++
@@ -1,7 +0,0 @@
-def rate_sort(user):
-    return user.rate
-
-def select_top_users_by_rate(users, top_size):
-    selected_users = sorted(users, key=rate_sort, reverse=True)
-
-    return selected_users[:top_size]
